2300aefa22d421b965b919e3cbfc48ab305ad07f
enginoobz <enginoobz@gmail.com>
2021-11-27 21:44:06 +0100

    [FLYWEIGHT] Unity implementation: Prefab & ScriptableObject

--- Assets/Design Patterns/Flyweight Pattern/Implementations/Unity-Specific/Prefab/BoidsController.cs
+++ Assets/Design Patterns/Flyweight Pattern/Implementations/Unity-Specific/Prefab/BoidsController.cs
@@ -1,14 +1,15 @@
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 namespace Flyweight.Unity.Prefab {
   public class BoidsController : Flyweight.BoidsController {
-    [SerializeField]
-    private List<Boid> boidPrefabs = new List<Boid>();
+    [SerializeField, InlineEditor]
+    private List<Flyweight.Boid> boidPrefabs = new List<Flyweight.Boid>();
 
     protected override void SpawnBoids() {
       for (int i = 0; i < boidAmount; i++) {
-        Boid newBoid = Instantiate(boidPrefabs.GetRandom(), RandomPosInCage, RandomRot);
+        Flyweight.Boid newBoid = Instantiate(boidPrefabs.GetRandom(), RandomPosInCage, RandomRot);
         _boids.Add(newBoid);
       }
     }
