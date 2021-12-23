b757af41c0d97fc73579860cc62d8826ef028058
enginoobz <enginoobz@gmail.com>
2021-12-23 14:22:06 +0100

    [BRIDGE] Rename naive implementation 1: flatten hierarchy

--- Assets/Design Patterns/Bridge/Case Study 1 - Shape, Size & Skin/Implementation Base/Scripts [Abstraction - Shape]/Shape.cs
+++ Assets/Design Patterns/Bridge/Case Study 1 - Shape, Size & Skin/Implementation Base/Scripts [Abstraction - Shape]/Shape.cs
@@ -7,13 +7,14 @@ namespace BridgePattern.Case1.Base {
   public abstract class Shape {
     protected ISize _size;
     protected ISkin _skin;
+
     public void SetSize(ISize size) => _size = size;
     public void SetSkin(ISkin skin) => _skin = skin;
 
     public void Display() {
-      GameObject shape = CreateGameObject();
-      _size.ProcessSize(shape);
-      _skin.ProcessSkin(shape);
+      GameObject go = CreateGameObject();
+      _size?.ProcessSize(go);
+      _skin?.ProcessSkin(go);
     }
 
     protected abstract GameObject CreateGameObject();
